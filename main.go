c595de988aa04d4aa41ce637c2ad7a9422e83c4a
Kyle Maxwell <krmaxwell@gmail.com>
2015-05-06 21:44:00 +0000

    Implement basic server

--- main.go
+++ main.go
@@ -24,7 +24,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// get the room going
+	go r.run()
 	// start the web server
 	if err := http.ListenAndServe(":6060", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
