f0e72139e3c6c28a7f9d4238ccc8b4e06df3e848
Kyle Maxwell <krmaxwell@gmail.com>
2015-05-06 21:17:41 +0000

    Use a template and handler

--- main.go
+++ main.go
@@ -3,22 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-      <head>
-      <title>Chat</title>
-      </head>
-      <body>
-      Let's chat!
-      [ 10 ]Chapter 1
-      </body>
-      </html>
-      `))
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// start the web server
 	if err := http.ListenAndServe(":6060", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
